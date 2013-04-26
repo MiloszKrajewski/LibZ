baf0bf971219a6f0735552f95a684db7ac8696b5
Krashan_cp <SND\Krashan_cp>
2013-04-26 08:04:34 -0700

    cleanup

--- trunk/LibZ.Codec.LZ4/postbuild.cmd
+++ trunk/LibZ.Codec.LZ4/postbuild.cmd
@@ -2,7 +2,5 @@
 setlocal
 set LIBZ=..\..\..\libz\bin\Release\libz.exe
 del /q lz4.libzcodec 2> nul
-%LIBZ% add --libz lz4.libzcodec --codec deflate --move --include *.dll
-%LIBZ% list --libz lz4.libzcodec
-%LIBZ% rebuild --libz lz4.libzcodec
+%LIBZ% add --libz lz4.libzcodec --include *.dll --codec deflate --move
 xcopy /y /d lz4.libzcodec ..\..\..\libz\bin\Release\
--- trunk/LibZ.Codec.ZLib/postbuild.cmd
+++ trunk/LibZ.Codec.ZLib/postbuild.cmd
@@ -2,7 +2,5 @@
 setlocal
 set LIBZ=..\..\..\libz\bin\Release\libz.exe
 del /q zlib.libzcodec 2> nul
-%LIBZ% add --libz zlib.libzcodec --codec deflate --move --include *.dll
-%LIBZ% list --libz zlib.libzcodec
-%LIBZ% rebuild --libz zlib.libzcodec
+%LIBZ% add --libz zlib.libzcodec --include *.dll --codec deflate --move 
 xcopy /y /d zlib.libzcodec ..\..\..\libz\bin\Release\
