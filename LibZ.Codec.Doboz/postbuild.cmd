9f5993032ec9adf9062b02e113931a09a925cac3
Milosz Krajewski <miloszkrajewski@o2.pl>
2013-08-17 23:30:55 +0100

    cleanup (binary identical)

--- LibZ.Codec.Doboz/postbuild.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set LIBZ=..\..\..\libz\bin\Release\libz.exe
-del /q doboz.libzcodec 2> nul
-%LIBZ% add --libz doboz.libzcodec --include *.dll --codec deflate --move
-xcopy /y /d doboz.libzcodec ..\..\..\libz\bin\Release\
